24a1197f798dbe3222688f6d3fd01919d986a53f
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-09-21 13:08:49 +0530

    modify

--- src/Dashboard/Header/index.js
+++ src/Dashboard/Header/index.js
@@ -21,9 +21,12 @@ function Header() {
 
 
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar  variant="dark" class="navbar navbar-dark bg-light">
       <Container>
-        <Form className="d-flex">
+        <div>
+      <img src={require("../../Assests/logo.png").default}  height="120px"  class="d-inline-block align-top" alt=""></img>
+      </div>
+        {/* <Form className="d-flex">
           <FormControl
             type="search"
             placeholder="Search"
@@ -31,12 +34,12 @@ function Header() {
             aria-label="Search"
           />
           <Button variant="p-3 mb-2 bg-light text-dark">Search</Button>
-        </Form>
+        </Form> */}
         <Nav className="me-auto">
           {/* <Nav.Link><Button variant="p-3 mb-2 bg-light text-dark" onClick={logout}>Logout</Button></Nav.Link> */}
           <Nav.Link>
             {user ? <img src={user.photoURL} height="35px" border-radius="8px" /> : <Button variant="p-3 mb-2 bg-light text-dark" onClick={signInBtnClick}>Signin with google</Button>}
-            <Button variant="p-3 mb-2 bg-light text-dark" onClick={signInBtnClick}>Signin with google</Button>
+            <Button class="btn btn-info" onClick={signInBtnClick}>Signin with google</Button>
           </Nav.Link>
         </Nav>
       </Container>
